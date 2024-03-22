239107670b95ef961a257035c4667208cce84b3e
Abhishek Arya <aarya@atlassian.com>
2024-03-22 23:39:45 +0530

    added create customer and accounts logic

--- accounts/src/main/java/com/bank/accounts/impl/AccountsServiceImpl.java
+++ accounts/src/main/java/com/bank/accounts/impl/AccountsServiceImpl.java
@@ -1,6 +1,10 @@
 package com.bank.accounts.impl;
 
+import com.bank.accounts.constants.AccountsConstants;
 import com.bank.accounts.dto.CustomerDto;
+import com.bank.accounts.entity.Accounts;
+import com.bank.accounts.entity.Customer;
+import com.bank.accounts.mapper.CustomerMapper;
 import com.bank.accounts.repository.AccountsRepository;
 import com.bank.accounts.repository.CustomerRepository;
 import com.bank.accounts.service.IAccountsService;
@@ -16,6 +20,20 @@ public class AccountsServiceImpl implements IAccountsService {
 
     @Override
     public void createAccount(CustomerDto customerDto) {
+        Customer customer = CustomerMapper.mapToCustomer(customerDto, new Customer());
+        // Customer and newAccount are created in the same transaction
+        customerRepository.save(customer);
+        accountsRepository.save(createNewAccount(customer));
+    }
 
+    private Accounts createNewAccount(Customer customer) {
+        Accounts newAccount = new Accounts();
+        newAccount.setCustomerId(customer.getCustomerId());
+        long randomAccountNumber = (long) (Math.random() * 10000000000000000L);
+
+        newAccount.setAccountNumber(randomAccountNumber);
+        newAccount.setAccountType(AccountsConstants.SAVINGS);
+        newAccount.setBranchAddress(AccountsConstants.BRANCH_ADDRESS);
+        return newAccount;
     }
 }
