5651774bbab42ad8e0733484d065b72ede760deb
Abhishek Arya <aarya@atlassian.com>
2024-03-28 18:53:27 +0530

    added open api schema definition

--- accounts/src/main/java/com/bank/accounts/dto/AccountsDto.java
+++ accounts/src/main/java/com/bank/accounts/dto/AccountsDto.java
@@ -1,19 +1,33 @@
 package com.bank.accounts.dto;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.NotEmpty;
 import jakarta.validation.constraints.Pattern;
 import lombok.Data;
 
 @Data
+@Schema(
+        name = "Accounts",
+        description = "Schema to hold Account information"
+)
 public class AccountsDto {
 
+    @Schema(
+            description = "Account Number of John Doe account", example = "1234567890"
+    )
     @NotEmpty(message = "Account number can not be a null or empty")
     @Pattern(regexp = "(^$|[0-9]{10})", message = "Mobile number must be 10 digits")
     private Long accountNumber;
 
+    @Schema(
+            description = "Account type of John Doe account", example = "Savings"
+    )
     @NotEmpty(message = "Account type can not be a null or empty")
     private String accountType;
 
+    @Schema(
+            description = "John Doe branch address", example = "123, 4th Cross, 5th Main, Bangalore"
+    )
     @NotEmpty(message = "Branch address can not be a null or empty")
     private String branchAddress;
 
--- accounts/src/main/java/com/bank/accounts/dto/ErrorResponseDto.java
+++ accounts/src/main/java/com/bank/accounts/dto/ErrorResponseDto.java
@@ -1,16 +1,36 @@
 package com.bank.accounts.dto;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import org.springframework.http.HttpStatus;
 
 import java.time.LocalDateTime;
 
+@Schema(
+        name = "ErrorResponse",
+        description = "Schema to hold Error Response information"
+)
 @Data @AllArgsConstructor
 public class ErrorResponseDto {
-
+    // Not mentioning default or example values, since the error will keep on changing
+    @Schema(
+            description = "API Path where the error occurred"
+    )
     private String apiPath;
+
+    @Schema(
+            description = "HTTP Status code of the error"
+    )
     private HttpStatus errorCode;
+
+    @Schema(
+            description = "Error message of the error"
+    )
     private String errorMessage;
+
+    @Schema(
+            description = "Time when the error occurred"
+    )
     private LocalDateTime errorTime;
 }
